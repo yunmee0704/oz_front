b9d69db2f3e2374720485a94e4700405a07befe5
yunmee0704 <900704630c@gmail.com>
2024-01-04 00:27:49 +0900

    주석추가하기

--- 3.js/17~18일차/todolist/todolist.js
+++ 3.js/17~18일차/todolist/todolist.js
@@ -19,15 +19,17 @@ function loadTodos(){
     }  
 }
 
-loadTodos()
+loadTodos()//로드 되면 로컬저장소에서 가져온 것 보여주기
 
 //할일 삭제하기
-function handleTodoDelBtnClick(clickedId){
+function handleTodoDelBtnClick(clickedId){    
+   
     todoArr = todoArr.filter(function(aTodo){
-        return aTodo.todoId !== clickedId
+        return aTodo.todoId !== clickedId//배열의 각 자료들의 todoId를 비교해서 둘 다 다른 것들만 정제하여 갱신하기        
     })
-    displayTodos()
-    saveTools()
+    
+    displayTodos()//보여주기
+    saveTools()//로컬저장소에 저장하기
 }
 
 //한일 완료 체크하기
@@ -49,46 +51,53 @@ function handleTodoItemClick(clickedId){
 //할일 보여주기
 function displayTodos(){
     todoList.innerHTML=''//초기화 한 후에 진행함. 안그러면 누적된 데이터가 매번 보여짐
-    todoArr.forEach(function(aTodo){
-        const todoItem = document.createElement('li')
+
+    todoArr.forEach(function(aTodo){//로컬스토리지에서 가져온 투두리스트의 자료들만큼
+        const todoItem = document.createElement('li')//li 생성
         const todoDelBtn = document.createElement('span')
-        todoDelBtn.textContent='삭제'
-        todoItem.textContent = aTodo.todoText
-        todoItem.title='클릭하면 완료됨'
-        
-        if(aTodo.todoDone){
-            todoItem.classList.add('done')
-        }else{
-            todoItem.classList.add('yet')
-        }
+        todoDelBtn.textContent='삭제'//그 안에 들어갈 삭제버튼 생성
+        todoItem.textContent = aTodo.todoText //li안에 들어갈 내용은 투두리스트 배열의 각 자료의 내용
+        todoItem.title='클릭하면 완료됨'//li위에 마우스를 올리면 완료하는 방법 나옴
+        todoDelBtn.title='클릭하면 삭제됨'//삭제버튼 위에 마우스 올리면 삭제하는법 설명
 
-        todoDelBtn.title='클릭하면 삭제됨'
+        // 완료스타일
+        if(aTodo.todoDone){//만약 todoDone이 true가 되면
+            todoItem.classList.add('done')//done클래스를 붙여 완료표시 스타일 줌
+        }else{ //아니면
+            todoItem.classList.add('yet')//done클래스 안의 스타일을 사라지게.
+        }        
 
+        // 완료표시
         todoItem.addEventListener('click',function(){
             handleTodoItemClick(aTodo.todoId);
         })
 
+        // 삭제
         todoDelBtn.addEventListener('click',function(){
             
             handleTodoDelBtnClick(aTodo.todoId)
+         
         })
 
-        todoItem.appendChild(todoDelBtn)
-        todoList.appendChild(todoItem)
+
+        todoItem.appendChild(todoDelBtn)//삭제버튼 추가
+        todoList.appendChild(todoItem)//새로운 내용 추가
     })
 }
 
 //할일 추가하기
 todoForm.addEventListener('submit',function(e){
     e.preventDefault()//submit는 페이지가 새로고침하는 기능이 있어서 방지.
+
+    // 내용,자료별 고유아이디, 할일 완료여부를 담은 자료를 객체로 저장함
     const toBeAdded ={
         todoText:todoForm.todo.value,//인풋창에 입력한 내용
         todoId:new Date().getTime(),
         todoDone: false//다 하지 않은 상태
     }
+
     todoForm.todo.value=''//입력 후 인풋창 초기화
-    todoArr.push(toBeAdded)
-    console.log(todoArr)
-    displayTodos()
-    saveTools()
+    todoArr.push(toBeAdded)//자료배열에 새로운 자료 객체 추가
+    displayTodos()//보여주기
+    saveTools()//로컬스토리지에 저장
 })
